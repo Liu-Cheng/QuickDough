c5b618f7c7ad2d59324a81f3b77e923e183a5122
Cheng Liu <st.liucheng@gmail.com>
2013-07-31 17:09:49 +0800

    Add DFGDump.h

---
+++ DFGDump/src/DFGDump.h
@@ -0,0 +1,61 @@
+//===- DFGDump.h - Head file of DFGDUmp.cpp--------------- ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements OPNode for DFGDump.cpp.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DFG_DUMP_H
+#define DFG_DUMP_H
+
+#include <list>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+namespace llvm{
+    struct OPNode{
+        unsigned int OriginalID;
+        unsigned int NewID;
+        std::string NodeName;
+        std::list<OPNode*> NodePred;
+        bool Accessed;
+        bool ShowInDFG;
+        bool IsConst;
+        bool IsInput;
+        bool IsResult;
+        std::string OriginalName; //Name in original program
+    };
+
+    /// Keeps all the details of an element.
+    /// Maximum dimension of the array is 2.
+    /// When it is a vector, ydim=-1;
+    struct elementInfo{
+        public:
+            std::string arrayName;
+            int xdim;
+            int ydim;
+
+            elementInfo(std::string _arrayName, int _xdim, int _ydim){
+                arrayName=_arrayName;
+                xdim=_xdim;
+                ydim=_ydim;
+            }
+
+            elementInfo(){
+                arrayName="";
+                xdim=-1;
+                ydim=-1;
+            }
+    };
+
+}
+
+#endif
--- DFGDump/src/Readme.txt
+++ DFGDump/src/Readme.txt
@@ -1,3 +1,6 @@
 1) Put the source code to llvmsrc directory.
 mv ./src llvmsrc/lib/Transform/DFGDump
-
+cd llvmsrc/include/llvm/
+mkdir DFGDump
+cd DFGDump
+cp ./src/DFGDump.h .
