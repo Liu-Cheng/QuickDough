55de282e7752efaac96363cadf2a2f7a858fc873
Liu-Cheng <st.liucheng@gmail.com>
2016-06-27 23:03:10 +0800

    update scgra project

---
+++ scgra2x2-1k/scgra2x2_1k.sdk/SDK/SDK_Export/kmean-small/Debug/src/kmean.d
@@ -0,0 +1,85 @@
+src/kmean.d: ../src/kmean.c \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/common.h ../../kmean-small_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xdmaps.h \
+ ../../kmean-small_bsp/ps7_cortexa9_0/include/xdmaps_hw.h \
+ ../src/src_ctrl_words.h ../src/result_ctrl_words.h ../src/io.h
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/common.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xdmaps.h:
+
+../../kmean-small_bsp/ps7_cortexa9_0/include/xdmaps_hw.h:
+
+../src/src_ctrl_words.h:
+
+../src/result_ctrl_words.h:
+
+../src/io.h:
---
+++ scgra2x2-1k/scgra2x2_1k.sdk/SDK/SDK_Export/mm-large/Debug/src/mm.d
@@ -0,0 +1,85 @@
+src/mm.d: ../src/mm.c \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/common.h ../../mm-large_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xdmaps.h \
+ ../../mm-large_bsp/ps7_cortexa9_0/include/xdmaps_hw.h \
+ ../src/src_ctrl_words.h ../src/result_ctrl_words.h ../src/io.h
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/common.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xdmaps.h:
+
+../../mm-large_bsp/ps7_cortexa9_0/include/xdmaps_hw.h:
+
+../src/src_ctrl_words.h:
+
+../src/result_ctrl_words.h:
+
+../src/io.h:
